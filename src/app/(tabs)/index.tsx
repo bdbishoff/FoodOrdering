c0f4b4672fa9d71fba84e9fb0b2a35c61318651c
Skrub <44077242+triplebproductionzz@users.noreply.github.com>
2025-01-14 17:55:30 -0700

    added scroll view of main page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,37 +1,16 @@
-import Colors from "@/src/constants/Colors";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native";
 import products from "../../../assets/data/products";
+import { ProductListItem } from "../../components/ProductListItem";
 
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image }}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}</Text>
-    </View>
+    <ScrollView>
+      {products.map((product) => (
+        <ProductListItem
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { backgroundColor: "white", padding: 10, borderRadius: 10 },
-  title: {
-    fontSize: 18,
-    fontWeight: 600,
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: { width: "100%", aspectRatio: 1 },
-});
